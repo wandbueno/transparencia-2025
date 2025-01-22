3b03b1d1d78d3f2b127497fa951e20ca487a50e4
Wanderson Bueno <wanolibueno@gmail.com>
2025-01-21 22:56:20 -0300

    pagina de diarias finalizada

--- client/src/services/filters/diarias/index.js
+++ client/src/services/filters/diarias/index.js
@@ -3,21 +3,6 @@ export const DIARIAS_COMBO_FILTERS = ['orgao']
 
 export const DIARIAS_TEXT_FIELDS = [
   {
-    id: 'matriculaDoFuncionario',
-    label: 'Matrícula',
-    type: 'text'
-  },
-  {
-    id: 'nomeDoFuncionario',
-    label: 'Nome do Funcionário',
-    type: 'text'
-  },
-  {
-    id: 'destino',
-    label: 'Destino',
-    type: 'text'
-  },
-  {
     id: 'dataInicial',
     label: 'Data Inicial',
     type: 'date'
@@ -30,22 +15,67 @@ export const DIARIAS_TEXT_FIELDS = [
   {
     id: 'numeroDaLiquidacao',
     label: 'Número da Liquidação',
+    type: 'number'
+  },
+  {
+    id: 'matriculaDoFuncionario',
+    label: 'Matrícula',
+    type: 'number'
+  },
+  {
+    id: 'nomeDoFuncionario',
+    label: 'Nome do Funcionário',
     type: 'text'
+  },
+  {
+    id: 'numeroDoProcesso',
+    label: 'Número do Processo',
+    type: 'number'
+  },
+  {
+    id: 'numeroDaPortaria',
+    label: 'Número da Portaria',
+    type: 'number'
+  },
+  {
+    id: 'anoDaPortaria',
+    label: 'Ano da Portaria',
+    type: 'number'
   },
   {
     id: 'valor',
     label: 'Valor',
+    type: 'number'
+  },
+  {
+    id: 'numeroDoEmpenho',
+    label: 'Número do Empenho',
+    type: 'number'
+  },
+  {
+    id: 'numeroDoPagamento',
+    label: 'Número do Pagamento',
+    type: 'number'
+  },
+  {
+    id: 'destino',
+    label: 'Local de Destino',
     type: 'text'
   }
 ]
 
 export const DIARIAS_CUSTOM_WIDTHS = {
-  orgao: '25%',
-  matriculaDoFuncionario: '25%',
-  nomeDoFuncionario: '50%',
-  destino: '50%',
+  orgao: '50%',
   dataInicial: '25%',
   dataFinal: '25%',
   numeroDaLiquidacao: '25%',
-  valor: '25%'
+  matriculaDoFuncionario: '25%',
+  nomeDoFuncionario: '50%',
+  numeroDoProcesso: '25%',
+  numeroDaPortaria: '25%',
+  anoDaPortaria: '25%',
+  valor: '25%',
+  numeroDoEmpenho: '25%',
+  numeroDoPagamento: '25%',
+  destino: '50%'
 }
