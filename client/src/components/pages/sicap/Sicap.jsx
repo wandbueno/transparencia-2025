82700631a8750f973467edb04f54614dcf5963e7
Wanderson Bueno <wanolibueno@gmail.com>
2025-01-31 18:28:02 -0300

    Resolução de conflitos ao fazer merge com a feature

--- client/src/components/pages/sicap/Sicap.jsx
+++ client/src/components/pages/sicap/Sicap.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { getProcedimentos } from "../../../services/sicap/procedimentos";
 import DataTableComponent from "../../common/DataTable";
-import PageHeader from '../../common/PageHeader';
-
-import InfoText from '../../common/InfoText';
-import LoadingSpinner from '../../common/LoadingSpinner';
-import { config } from '../../../assets/config';
-import { Link } from 'react-router-dom';
+import PageHeader from "../../common/PageHeader";
+import InfoText from "../../common/InfoText";
+import LoadingSpinner from "../../common/LoadingSpinner";
+import { config } from "../../../assets/config";
+import { Link } from "react-router-dom";
 import ButtonTable from "../../common/ButtonTable";
 
 // Função para truncar textos longos
 export const truncateText = (text, maxLength) => {
-  if (!text) return '';
+  if (!text) return "";
   if (text.length <= maxLength) return text;
   return `${text.substring(0, maxLength)}...`;
 };
@@ -19,18 +18,18 @@ export const truncateText = (text, maxLength) => {
 // Função para retornar a cor de acordo com a fase
 const getColorForFase = (fase) => {
   switch (fase) {
-    case '1ªF':
-      return '#428bca';
-    case '2ªF':
-      return '#ffd700';
-    case 'CO':
-      return '#2e8b57';
-    case 'TE':
-      return '#b22222';
-    case 'OB':
-      return '#daa520';
+    case "1ªF":
+      return "#428bca";
+    case "2ªF":
+      return "#ffd700";
+    case "CO":
+      return "#2e8b57";
+    case "TE":
+      return "#b22222";
+    case "OB":
+      return "#daa520";
     default:
-      return 'gray'; // Cor padrão se a fase não for reconhecida
+      return "gray"; // Cor padrão se a fase não for reconhecida
   }
 };
 
@@ -38,7 +37,8 @@ const getColorForFase = (fase) => {
 const renderFases = (fases) => {
   if (!fases) return null;
 
-  const fasesArray = fases.split(' '); // Separar as fases por espaço
+  // Se fases for uma string, converte para array; caso contrário, presume que já é um array.
+  const fasesArray = typeof fases === "string" ? fases.split(" ") : fases;
 
   return fasesArray.map((fase, index) => (
     <span
@@ -46,80 +46,75 @@ const renderFases = (fases) => {
       className="fase"
       style={{
         backgroundColor: getColorForFase(fase),
-        color: 'white',
-        padding: '4px 8px',
-        borderRadius: '5px',
-        marginRight: '5px',
-        display: 'inline-block',
-        fontSize: '11px'
+        color: "white",
+        padding: "4px 8px",
+        borderRadius: "5px",
+        marginRight: "5px",
+        display: "inline-block",
+        fontSize: "11px",
       }}
     >
       {fase}
     </span>
   ));
 };
+
 // Definindo as colunas da tabela
 const columnsProcedimentos = [
-  { 
-    name: "Tipo | Modalidade", 
-    selector: (row) => row.tipoExecucao, 
-    sortable: true, 
-    width: '12%' 
+  {
+    name: "Tipo | Modalidade",
+    selector: (row) => row.tipoExecucao,
+    sortable: true,
+    width: "12%",
   },
-  { 
-    name: "Processo", 
-    selector: (row) => row.processoAdministrativo, 
-    sortable: true, 
-    width: '8%' 
+  {
+    name: "Processo",
+    selector: (row) => row.processoAdministrativo,
+    sortable: true,
+    width: "8%",
   },
-  { 
-    name: "Cadastro", 
-    selector: (row) => row.dataCadastro, 
-    sortable: true, 
-    width: '8%' 
+  {
+    name: "Cadastro",
+    selector: (row) => row.dataCadastro,
+    sortable: true,
+    width: "8%",
   },
-  { 
-    name: "Abertura", 
-    selector: (row) => row.dataAbertura, 
-    sortable: true, 
-    width: '10%' 
+  {
+    name: "Abertura",
+    selector: (row) => row.dataAbertura,
+    sortable: true,
+    width: "10%",
   },
-  { 
-    name: "Valor", 
-    selector: (row) => row.valor, 
-    sortable: true, 
-    width: '10%' 
+  {
+    name: "Valor",
+    selector: (row) => row.valor,
+    sortable: true,
+    width: "10%",
   },
   {
     name: "Situação",
     selector: (row) => row.fases,
-    cell: (row) => renderFases(row.fases), // Renderizar as fases coloridas
-    width: '12%'
+    cell: (row) => renderFases(row.fases),
+    width: "14%",
   },
-  { 
-    name: "Objeto", 
-    selector: (row) => row.descricaoObjeto, 
-    sortable: true, 
-    width: '29%',
+  {
+    name: "Objeto",
+    selector: (row) => row.descricaoObjeto,
+    sortable: true,
+    width: "27%",
     cell: (row) => (
-      <div className="break-word">
-        {truncateText(row.descricaoObjeto, 100)}
-      </div>
-    )
+      <div className="break-word">{truncateText(row.descricaoObjeto, 100)}</div>
+    ),
   },
   {
     name: "Detalhes",
     selector: (row) => row.id,
     cell: (row) => (
-      <ButtonTable 
-        path="/sicap"
-        id={row.id}
-        label="Ver Detalhes"
-      />
+      <ButtonTable path="/sicap" id={row.id} label="Ver Detalhes" />
     ),
-    width: '11%',
-    excludeFromExport: true
-  }
+    width: "11%",
+    excludeFromExport: true,
+  },
 ];
 
 const Sicap = () => {
@@ -129,7 +124,7 @@ const Sicap = () => {
 
   useEffect(() => {
     document.title = `SICAP Procedimentos - Portal Transparência - ${config.geral.nomeOrgao}`;
-    
+
     const fetchData = async () => {
       setLoading(true);
       try {
@@ -151,18 +146,16 @@ const Sicap = () => {
       <PageHeader
         title="SICAP Procedimentos"
         breadcrumb={[
-          { label: 'Página Inicial', path: '/' },
-          { label: 'Transparência', path: '/sicap' },
-          { label: 'SICAP Procedimentos' },
+          { label: "Página Inicial", path: "/" },
+          { label: "Transparência", path: "/sicap" },
+          { label: "SICAP Procedimentos" },
         ]}
       />
-      
-      
 
       <InfoText>
         <p>Veja Procedimentos de Licitação aqui</p>
       </InfoText>
-         
+
       {loading ? (
         <LoadingSpinner />
       ) : error ? (
